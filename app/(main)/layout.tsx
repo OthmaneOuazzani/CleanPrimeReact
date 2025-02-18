ae7864dadff9718cad39f921d608414dd17f9f13
OUAZZANI TOUHAMI <othmane.ouazzani-touhami@capgemini.com>
2025-02-18 18:59:09 +0100

    Cleaned template

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+import Layout from '../../layout/layout';
+
+interface AppLayoutProps {
+    children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: 'F1A .NET',
+    description: 'FullStack project for .NET FA1 junios devs',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    openGraph: {
+        type: 'website',
+        title: 'FA1 FullStack Project',
+        url: '',
+        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+        images: [''],
+        ttl: 604800
+    },
+    icons: {
+        icon: '/favicon.ico'
+    }
+};
+
+export default function AppLayout({ children }: AppLayoutProps) {
+    return <Layout>{children}</Layout>;
+}
